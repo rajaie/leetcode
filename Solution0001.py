26760a9ba2888c6aa78aa9078c467d2452d5756a
Rajaie <rajaie.alkorani@gmail.com>
2017-02-06 22:44:31 -0500

    Initial commit

---
+++ Solution0001.py
@@ -0,0 +1,39 @@
+"""
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome. 
+"""
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if s == "":
+            return True
+        
+        s = s.lower()
+        i=0
+        j=len(s)-1
+        
+        while i < j:
+            while not s[i].isalnum():
+                i += 1
+                if i == len(s):
+                    return True
+            while not s[j].isalnum():
+                j -= 1
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+    
+        return True
